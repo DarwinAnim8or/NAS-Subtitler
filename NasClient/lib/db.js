b5113a0e0243b53bf6cae68ad3e4d990160b568c
17600387682@163.com <hao1998...>
2025-09-30 16:42:31 +0800

    删除nasclient

--- NasClient/lib/db.js
+++
@@ -1,8 +0,0 @@
-// Prisma singleton instance
-const { PrismaClient } = require('@prisma/client');
-// Ensure config side-effects (DATABASE_URL) applied before instantiation
-require('./config');
-
-const prisma = new PrismaClient();
-
-module.exports = { prisma };
--- NasClient/prisma/migrations/20250913101022_add_video_sub_exts/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "settings" ADD COLUMN "baseRel" TEXT;
-ALTER TABLE "settings" ADD COLUMN "subExts" TEXT;
-ALTER TABLE "settings" ADD COLUMN "videoExts" TEXT;
